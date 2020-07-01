b37d28a58711189ea6af8c901a909f0b63a13902
NotQuestions <kipen.vasyl@gmail.com>
2020-07-01 21:03:17 +0300

    - HW_1 Angular

---
+++ src/app/components/comment/comment.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Comment} from '../../models/Comment';
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.css']
+})
+export class CommentComponent  {
+  @Input()
+  comment: Comment;
+
+}
+
